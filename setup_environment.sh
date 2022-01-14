4922d43003db2c96c7a206cdf1d40de1a7ae0857
Luis Perera Lopez <luis.pereralopez@ukg.com>
2022-01-14 10:54:52 -0500

    2022-01-14 10:54:52

--- setup_environment.sh
+++ setup_environment.sh
@@ -2,6 +2,32 @@
 
 ############################### FUNCTIONS ###############################
 
+# prompts a yes/no question to install software
+# $1 -> name of the software to install
+# $2 -> command to execute to install the software
+yes_no_question() {
+    while true; do
+        read -p "Do you want to install $1? (y/n) " yn
+        case $yn in
+            [Yy]* ) echo "Installing $1..."
+                    $2
+                    break;;
+            [Nn]* ) break;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
+# check https://docs.ulti.io/systems/quark/getting-started/install-cli/ for installation instructions
+install_quark_cli () {
+    curl -L -o /tmp/qrk-v3.0.20.pkg https://artifactory.mia.ulti.io/artifactory/gt-generic-prod/quark-cli/installers/macos/qrk-v3.0.20.pkg
+    echo "Termial needs permission to install package 'qrk-v3.0.20.pkg', please enter user password:"
+    sudo installer -pkg /tmp/qrk-v3.0.20.pkg -tgt /
+    qrk update
+    qrk setup
+    rm -rf /tmp/qrk-v3.0.20.pkg
+}
+
 install_required() {
     #####
     # Required Installations
@@ -42,6 +68,15 @@ install_required() {
 
     # install Docker
     brew install --cask docker
+
+    # install Kubectl
+    brew install kubectl 
+
+    # install Quark CLI
+    install_quark_cli
+
+    # install Strat CLI (https://docs.ulti.io/systems/cloud-engine/kubernetes/stratcli)
+    sh -c "$(https://ultigit.ultimatesoftware.com/projects/STRAT/repos/stratcli/raw/hack/install.sh?at=refs%2Fheads%2Fmaster)"
 }
 
 install_optional() {
@@ -70,24 +105,11 @@ install_optional() {
     # install Mozilla Firefox
     yes_no_question "Mozilla Firefox" "brew install --cask firefox"
 
+    # install Keeper Password Manager
+    yes_no_question "Keeper Password Manager" "brew install --cask keeper-password-manager"
+
     # install Oh My Zsh
     yes_no_question "Oh My Zsh (shell)" "sh -c '$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)'"
-}
-
-# prompts a yes/no question to install software
-# $1 -> software to install
-# $2 -> command to execute to install the software
-yes_no_question() {
-    while true; do
-        read -p "Do you want to install $1? (y/n) " yn
-        case $yn in
-            [Yy]* ) echo "Installing $1..."
-                    $2
-                    break;;
-            [Nn]* ) break;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
 }
 
 print_required_software() {
@@ -100,13 +122,16 @@ print_required_software() {
     echo "* Node"
     echo "* Java Azul Zulu JDK 11"
     echo "* Java Adopt Open JDK 8"
-    echo "* ItelliJ IDEA (note: need to request software license)"
+    echo "* IntelliJ IDEA (note: need to request software license)"
     echo "* Slack"
     echo "* Docker"
+    echo "* Kubectl"
+    echo "* Quark CLI"
+    echo "* Strat CLI"
 }
 
 ############################### MAIN ###############################
 
-print_required_software
-yes_no_question "the above metioned software" "install_required"
-install_optional
+# print_required_software
+# yes_no_question "the above metioned software" "install_required"
+# install_optional
