661f53f012f392dafe29a282e9d55a8347345975
Luis Perera Lopez <luis.pereralopez@ukg.com>
2022-03-02 15:05:27 -0500

    2022-03-02 15:05:27

--- save-script.sh
+++ save-script.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 cd /Users/luisp/ist/projects/dev-config
 git checkout master
@@ -7,15 +7,12 @@ git pull
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 echo "[$timestamp] Starting script" >> log.txt
 
-# copy required files and folders to this repo
+# copy required files/folders to this repo
 cp ~/.zshrc /Users/luisp/ist/projects/dev-config/zsh-profile/.zshrc
 echo "[$timestamp] Successfully copied .zshrc file" >> log.txt
 
 cp ~/.p10k.zsh /Users/luisp/ist/projects/dev-config/zsh-profile/.p10k.zsh
 echo "[$timestamp] Successfully copied .p10k.zsh file" >> log.txt
-
-cp -R ~/ist/cmds /Users/luisp/ist/projects/dev-config/
-echo "[$timestamp] Successfully copied cmds folder" >> log.txt
 
 # push to git
 git add .
